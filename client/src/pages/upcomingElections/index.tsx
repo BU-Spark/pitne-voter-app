9bc10bd5df074755d0e913036d0ed2027a867cb7
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-05-28 16:16:11 -0400

    Update heading for upcomingElections Page

---
+++ client/src/pages/upcomingElections/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function UpcomingElections() {
+    return (
+        <div>
+            <div className='flex flex-col justify-center items-center p-4'>
+                <h1 className='text-blue-700 font-bold text-8xl '>LET’S VOTE!</h1>
+                <p className='p-5'>The most important information about Boston's municiple elections to help you naviagte your voting journey</p>
+            </div>
+        </div>
+    )
+}
