4c510b67ad4a5303ef99e1674012e4d13c6224fa
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-05-28 10:55:02 -0400

    Finish Setting Up Coding Environment

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from '../components/nav'
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <h1>Home</h1>
+    </div>
+  )
+}
---
+++ client/src/components/nav/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Nav: React.FC = () => {
+    return (
+        <p>hello </p>
+    );
+};
+
+export default Nav;
