6025a959f7ca34c505977f9b3c005eb05061dbf1
Arshnoorkc13 <akchadha@umass.edu>
2024-05-29 13:29:35 -0400

    Change focus in text

--- client/src/pages/voterInfo/index.tsx
+++ client/src/pages/voterInfo/index.tsx
@@ -12,13 +12,13 @@ export default function VoterInfo() {
 
             <div className='flex flex-col justify-center items-center p-4 my-4'>
                 <h1 className='font-semibold text-left'>Basic Voter Info</h1>
-                <p className='p-4 bg-blue-200 rounded-full w-1/2'>County: <strong>Suffolk</strong></p>
+                <p className='p-4 bg-blue-200 rounded-full w-1/2'><strong> County: </strong>Suffolk</p>
             </div>
 
             <div className='flex flex-col justify-center items-center p-4 my-4'>
                 <h1 className='font-semibold text-left'>Polling Location</h1>
                 <p className='mt-8 mb-4'>Reminder: You can vote at any polling location during the early voting period, but you must vote at the location below during election day.</p>
-                <p className='p-4 bg-blue-200 rounded-full w-1/2'>County: <strong>Suffolk</strong></p>
+                <p className='p-4 bg-blue-200 rounded-full w-1/2'><strong>Address:</strong> </p>
             </div>
         </div>
 
