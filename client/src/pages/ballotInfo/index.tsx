67dbc824575cda758b8a4ce70a4f608ab48444a8
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-03 10:20:11 -0400

    update Address

--- client/src/pages/ballotInfo/index.tsx
+++ client/src/pages/ballotInfo/index.tsx
@@ -1,3 +1,5 @@
+import ButtonFill from "@/components/button/ButtonFill"
+
 export default function BallotInfo() {
     return (
         <div className=''>
@@ -6,10 +8,24 @@ export default function BallotInfo() {
                 <h1 className='text-blue-700 font-bold text-6xl '>Ballot Info</h1>
             </div>
 
-            <div className='flex flex-col justify-left items-left'>
-                <h1 className='text-blue-700 font-bold text-6xl '>LEARN. PLAN.</h1>
-                <h1 className='font-semibold text-xl p-5 mt-2'>Explore the elections, candidates, and crucial issues personalized to your community.</h1>
+            <div className="flex justify-center">
+                <div className="flex flex-col items-start text-left">
+                    <h1 className="font-bold text-3xl">LEARN. PLAN.</h1>
+                    <h1 className="font-semibold text-xl mt-2">Explore the elections, candidates, and crucial issues personalized to your community.</h1>
+
+                    <div className='w-full '>
+                        <h1 className="font-semibold text-xl mt-4">Address</h1>
+                        <p className="text-lg mt-4 p-5 bg-blue-100  rounded-full"> 500 Cherokee RD <br /> Boston MA, 02215</p>
+                        <ButtonFill name='Change Address' link='/changeAddress' className='p-4 mt-4 rounded-full bg-blue-700 text-white' />
+                    </div>
+                </div>
             </div>
+
+            <div>
+                <h1 className="font-semibold text-xl mt-4">Upcoming Elections</h1>
+                <p className="text-lg mt-4 p-5 bg-blue-100 rounded-full"> November 2, 2021</p>
+            </div>
+
 
         </div>
     )
