1437f2d987b85140e5d2beeb2173df272514718c
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2024-06-03 14:14:22 -0400

    small apostrophe thing

--- client/src/pages/voterInfo/index.tsx
+++ client/src/pages/voterInfo/index.tsx
@@ -13,7 +13,7 @@ export default function VoterInfo() {
             <div className='flex flex-col justify-center items-center p-4 text-center my-4'>
                 <h1 className='text-blue-700 font-bold text-6xl '>Your Voter Info</h1>
                 <h1 className='font-semibold text-xl p-5 mt-2'>Here is everything you need to know about your <br />voter status and personal voting logistics!</h1>
-                <p className=''>Can’t vote in person or want to vote early?</p>
+                <p className=''>Can&#39;t vote in person or want to vote early?</p>
                 <ButtonFill name='Early Voting Options' link='/votingOptions' className='p-4 mt-4 rounded-full bg-blue-700 text-white border-blue-800 ' />
             </div>
 
