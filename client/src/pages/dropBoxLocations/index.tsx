691ab625d5b704d961274734156824e28db87e9f
Arshnoorkc13 <akchadha@umass.edu>
2024-06-03 16:03:50 -0400

    Map Added

--- client/src/pages/dropBoxLocations/index.tsx
+++ client/src/pages/dropBoxLocations/index.tsx
@@ -4,6 +4,9 @@ import '@/app/globals.css'
 import ButtonFill from '@/components/button/ButtonFill';
 import BoxField from '@/components/box/BoxField';
 import SearchIcon from '@mui/icons-material/Search';
+import { Box } from '@mui/material';
+import Image from 'next/image'
+
 
 export default function DropBoxLocations() {
     return (
@@ -21,6 +24,15 @@ export default function DropBoxLocations() {
                     </div>
                 </div>
             </div> 
+
+            <div className='flex flex-col justify-center items-center p-4 my-10'>
+                <Box component="img" sx={{height: 400, width: 400}}
+                alt="Map" src="/Map.png"/>
+            </div>
+
+            <div className='flex flex-col justify-center items-center p-4 my-6'>
+                <p className='p-4 bg-white rounded-full w-1/2 text-center'><strong> County: </strong>Suffolk</p>
+            </div>
 
             <div className='flex flex-col justify-center items-center p-4 my-10'>
                 <p className=' font-semibold p-4'>You may be wondering.....</p>
