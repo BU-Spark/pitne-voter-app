386b8c949c8dbc49305b4a1a2bc464ab7e5a7652
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-03 10:04:48 -0400

    creating Ballot Info Page

---
+++ client/src/pages/ballotInfo/index.tsx
@@ -0,0 +1,16 @@
+export default function BallotInfo() {
+    return (
+        <div className=''>
+
+            <div className='flex flex-col justify-center items-center p-4 text-center mb-10'>
+                <h1 className='text-blue-700 font-bold text-6xl '>Ballot Info</h1>
+            </div>
+
+            <div className='flex flex-col justify-left items-left'>
+                <h1 className='text-blue-700 font-bold text-6xl '>LEARN. PLAN.</h1>
+                <h1 className='font-semibold text-xl p-5 mt-2'>Explore the elections, candidates, and crucial issues personalized to your community.</h1>
+            </div>
+
+        </div>
+    )
+}
