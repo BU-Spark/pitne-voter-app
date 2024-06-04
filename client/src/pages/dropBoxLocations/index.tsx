0dc3d91438139328e45cc3989452d45e6f262329
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2024-06-04 10:56:19 -0400

    small documentation things to drop box

--- client/src/pages/dropBoxLocations/index.tsx
+++ client/src/pages/dropBoxLocations/index.tsx
@@ -11,13 +11,13 @@ import Image from 'next/image'
 export default function DropBoxLocations() {
     return (
         <div>
-            <div className='flex flex-col justify-center items-center p-4 text-center my-4'>
+            <div className='flex flex-col justify-center items-center p-4 text-center mb-10'>
                 <h1 className='text-blue-700 font-bold text-6xl '>Drop Box Locations</h1>
-                <p className=''>Find the nearest ballot drop-off station and early voting locations in your area:</p>
+                <h1 className='font-semibold text-xl p-5'>Find the nearest ballot drop-off station and early voting locations in your area</h1>
             </div>
 
            <div className='flex flex-col justify-center items-center p-4 my-10'>
-           <div className='flex items-center space-x-2'>
+                <div className='flex items-center space-x-2'>
                     <BoxField />
                     <div className='bg-orange-400 rounded-full p-2'>
                     <SearchIcon/>
@@ -26,12 +26,14 @@ export default function DropBoxLocations() {
             </div> 
             
             <div className='relative flex flex-row justify-center items-center p-4 m-10'  style={{ left: '-8%' }}>
-                <Box component="img" sx={{height: 400, width: 400}}
-                alt="Map" src="/Map.png"/>
+                <Box component="img" sx={{height: 400, width: 400}} alt="Map" src="/Map.png"/>
                 <div className='absolute flex flex-col justify-center items-center p-4'  style={{ top: '60%', left: '78%', transform: 'translate(-50%, -50%)' }}>
-                <p className='px-40 py-40 bg-gray-200 rounded-lg'><strong>Ballot Dropbox: </strong> 149 Dudley</p>
+                    <p className='px-40 py-40 bg-gray-200 rounded-lg'>
+                        <strong>Ballot Dropbox:</strong>
+                        149 Dudley
+                    </p>
                 </div>
-            </div>     
+            </div>
 
             <div className='flex flex-col justify-center items-center p-4 my-10'>
                 <p className=' font-semibold p-4'>You may be wondering.....</p>
@@ -39,6 +41,5 @@ export default function DropBoxLocations() {
                 <ButtonFill name='Basic Election Info' link='/upcomingElections' variant='outlined' className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800  hover:bg-gray-100' />
             </div>
         </div>
-
     )
 }
--- client/src/pages/votingOptions/index.tsx
+++ client/src/pages/votingOptions/index.tsx
@@ -4,7 +4,6 @@ import DropDownInfo from './DropDownInfo';
 const VotingOptions = () => {
     return (
         <div className=''>
-
             <div className='flex flex-col justify-center items-center p-4 text-center mb-10'>
                 <h1 className='text-blue-700 font-bold text-6xl '>Voting Options</h1>
                 <h1 className='font-semibold text-xl p-5'>Everything you need to know about your voting options made simple!</h1>
