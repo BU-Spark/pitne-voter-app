b10f8b47f0872a36f8836cd2a08b162612e35018
Arshnoorkc13 <akchadha@umass.edu>
2024-06-06 12:38:25 -0400

    rounded corners fixed

--- client/src/pages/ballotInfo/popUpBox.tsx
+++ client/src/pages/ballotInfo/popUpBox.tsx
@@ -8,7 +8,12 @@ type Props = {
 
 const PopUpBox = ({ open, onClose }: Props) => {
     return (
-        <Dialog open={open} onClose={onClose} >
+        <Dialog open={open} onClose={onClose} PaperProps={{
+            sx: {
+                borderRadius: '20px', 
+                padding: '8px'
+            },
+        }}>
             <DialogContent>
                 <p >Councilors are elected every two years by the citizens of Boston. The council is made up of four at-large councilors that represent the entire City, and nine district councilors that represent specific areas of the City. The City Council serves as a link between the citizens of Boston and their municipal government.</p>
             </DialogContent>
