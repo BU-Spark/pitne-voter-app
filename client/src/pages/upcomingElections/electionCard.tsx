95bb408c7bb500f1d9c21ba45ec2bc9e1de8c645
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-10 14:00:09 -0400

    done with formatting

--- client/src/pages/upcomingElections/electionCard.tsx
+++ client/src/pages/upcomingElections/electionCard.tsx
@@ -9,11 +9,32 @@ type Props = {
 
 
 export default function ElectionCard({ electionName = 'Preliminary Municipal Election', electionDate }: Props) {
+    const [displayElectionDate, setDisplayElectionDate] = useState('')
+    const [displayRegistrationDate, setDisplayRegistrationDate] = useState('')
 
     useEffect(() => {
-        if (electionDate !== undefined) {
-            console.log(electionName)
-            console.log(electionDate)
+
+        if (electionDate !== undefined && electionName !== undefined) {
+
+
+            const formattedElectionDate = new Date(electionDate).toLocaleDateString('en-US', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric'
+            });
+            setDisplayElectionDate(formattedElectionDate);
+
+            const registrationDate = new Date(electionDate);
+            registrationDate.setDate(registrationDate.getDate() - 10);
+            const formattedRegistrationDate = registrationDate.toLocaleDateString('en-US', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric'
+            });
+            setDisplayRegistrationDate(formattedRegistrationDate);
+            console.log(formattedElectionDate)
+            console.log(formattedRegistrationDate)
+
         }
 
     }, [electionName, electionDate])
@@ -21,13 +42,13 @@ export default function ElectionCard({ electionName = 'Preliminary Municipal Ele
     return (
         <div>
             <div className='bg-gray-100 m-4 rounded-xl grid grid-cols-2 p-4 max-w-lg align-center'>
-                <p className="font-semibold text-red-500 m-2 text-center ">SEPT 2 @ 5PM</p>
+                <p className="font-semibold text-red-500 m-2 text-center ">{displayRegistrationDate} @ 5PM</p>
                 <p className='m-2 text-left'>Deadline for registration of voters for {electionName}.</p>
             </div>
 
             <div className='bg-gray-100 mx-4 rounded-xl grid grid-cols-2 p-4 max-w-lg align-center'>
 
-                <p className="font-semibold text-red-500 m-2 text-center ">election Date from 7AM - 8PM</p>
+                <p className="font-semibold text-red-500 m-2 text-center ">{displayElectionDate} @ 7AM - 8PM</p>
                 <p className='m-2 text-left'>{electionName}.</p>
             </div>
         </div>
