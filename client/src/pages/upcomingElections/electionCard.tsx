47cd859094583477cfd78c6a9211f6e9d8928bb4
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-11 10:45:30 -0400

    finished

--- client/src/pages/upcomingElections/electionCard.tsx
+++ client/src/pages/upcomingElections/electionCard.tsx
@@ -41,18 +41,29 @@ export default function ElectionCard({ electionName = 'Preliminary Municipal Ele
     }, [electionName, electionDate]);
 
     return (
-        <div>
-            <div className='bg-gray-100 m-4 rounded-xl grid grid-cols-2 p-4 max-w-lg align-center'>
-                <p className="font-semibold text-red-500 m-2 text-center ">{displayRegistrationDate} @ 5PM</p>
-                <p className='m-2 text-left'>Deadline for registration of voters for {electionName}.</p>
-            </div>
+        <div className="container mx-auto p-4">
+            <div className="flex flex-col lg:flex-row gap-4">
+                <div className="event-card bg-gray-100 rounded-xl p-4 flex-1">
+                    <p className="font-semibold text-red-500 m-2 text-center">
+                        {displayRegistrationDate} @ 5PM
+                    </p>
+                    <p className="m-2 text-center">
+                        Deadline for registration of voters for <strong>{electionName}</strong>.
+                    </p>
+                </div>
 
-            <div className='bg-gray-100 mx-4 rounded-xl grid grid-cols-2 p-4 max-w-lg align-center'>
-
-                <p className="font-semibold text-red-500 m-2 text-center ">{displayElectionDate} @ 7AM - 8PM</p>
-                <p className='m-2 text-left'>{electionName}.</p>
+                <div className="event-card bg-gray-100 rounded-xl p-4 flex-1">
+                    <p className="font-semibold text-red-500 m-2 text-center">
+                        {displayElectionDate} @ 7AM - 8PM
+                    </p>
+                    <p className="m-2 text-center">
+                        <strong>{electionName}.</strong>
+                    </p>
+                </div>
             </div>
         </div>
+
+
 
 
 
