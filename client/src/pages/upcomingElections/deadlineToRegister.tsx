07cb6ca08151a931442454a1fe07387fd8787f9b
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-11 10:22:10 -0400

    update recent election

--- client/src/pages/upcomingElections/deadlineToRegister.tsx
+++ client/src/pages/upcomingElections/deadlineToRegister.tsx
@@ -9,10 +9,10 @@ type Props = {
 export default function DeadlineToRegister() {
     const [isLoading, setIsLoading] = useState(true);
     const [electionDates, setElectionDates] = useState([])
+    const [displayRegistrationDate, setDisplayRegistrationDate] = useState('')
 
     useEffect(() => {
         const fetchElectionDates = async () => {
-            console.log('here')
             setIsLoading(true);
             try {
                 const response = await fetch('http://localhost:1337/api/boston-municipal-election-dates', {
@@ -23,7 +23,7 @@ export default function DeadlineToRegister() {
                 });
                 if (response.ok) {
                     const electionData = await response.json();
-                    setElectionDates(electionData.data[0])
+                    setElectionDates(electionData.data)
                     setIsLoading(false);
 
                 } else {
@@ -37,21 +37,40 @@ export default function DeadlineToRegister() {
             }
         }
         fetchElectionDates();
-
     }, [])
 
     useEffect(() => {
         if (electionDates.length > 0) {
-            console.log(electionDates)
-            setIsLoading(false)
+            setIsLoading(false);
+
+            const sortedDates = electionDates
+                .map(date => new Date(date.attributes.ElectionDate))
+                .sort((a, b) => a - b);
+
+
+            var minDate = new Date(Math.min.apply(null, sortedDates));
+            sortedDates.map(date => {
+                if (date < minDate) {
+                    minDate = date;
+                }
+            });
+
+            minDate.setDate(minDate.getDate() - 10);
+            minDate.setHours(0, 0, 0, 0);
+            const formattedRegistrationDate = minDate.toLocaleDateString('en-US', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric'
+            });
+
+            setDisplayRegistrationDate(formattedRegistrationDate)
         }
-    }, [electionDates])
+    }, [electionDates]);
 
 
     return (
         <div>
-            <p className='text-red-500'>Deadline to register: <strong>Aug 24, 2024</strong> </p>
-
+            <p className='text-red-500'>Deadline to register: <strong>{displayRegistrationDate}</strong> </p>
         </div>
 
     )
