2c0f9e324bf5b018883d3bc5be5766f233ccd402
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-20 16:45:22 -0400

    change

---
+++ strapi/src/api/ballot-initiative/routes/ballot-initiative.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ballot-initiative router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ballot-initiative.ballot-initiative');
