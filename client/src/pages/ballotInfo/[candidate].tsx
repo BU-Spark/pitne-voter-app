a5c312cd0f1eead2bf71bc4ab6e46b98df9871ae
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2024-06-26 09:16:14 -0400

    fixed styling for Q&A and documented

--- client/src/pages/ballotInfo/[candidate].tsx
+++ client/src/pages/ballotInfo/[candidate].tsx
@@ -50,13 +50,13 @@ interface QuestionsAndAnswers {
 
 export default function Candidate() {
     const router = useRouter();
-
-
     const [candidateName, setCandidateName] = useState<string>('');
     const [allCandidateData, setAllCandidateData] = useState<CandidateDataObject[]>([])
     const [candidateData, setCandidateData] = useState<CandidateAttributes | null>(null);
     const [questionsAndAnswers, setQuestionsAndAnswers] = useState<QuestionsAndAnswers>({});
 
+
+    // Get candidate name from URL
     useEffect(() => {
         if (!router.isReady) return;
 
@@ -65,6 +65,8 @@ export default function Candidate() {
 
     }, [router.isReady]);
 
+
+    // Get candidate data from strapi
     useEffect(() => {
         const getData = async () => {
             try {
@@ -87,6 +89,8 @@ export default function Candidate() {
 
     }, []);
 
+
+    // Set the candidate data
     useEffect(() => {
         if (candidateName && allCandidateData) {
             const normalizedInput = (input: string) => input.replace(/\s+/g, '').toLowerCase();
@@ -99,14 +103,14 @@ export default function Candidate() {
                 setCandidateData(null); // Handle case where candidate is not found
             }
         }
-
     }, [allCandidateData, candidateName]);
 
     useEffect(() => {
         console.log(candidateData);
     }, [candidateData])
 
-    // Get filled out questions and answers and populate map
+
+    // Get filled out questions and answers from strapi and populate map
     useEffect(() => {
         if (candidateData) {
             const qaMap = Object.entries(candidateData)
@@ -128,66 +132,74 @@ export default function Candidate() {
 
     return (
         <div className="relative flex min-h-screen flex-col bg-[#d1e4fa] overflow-x-hidden">
-            
-                <header className="flex border-b border-solid border-b-white px-10 py-3"></header>
-                <div className="flex mt-6">
-                    <button
-                        type="button"
-                        onClick={() => router.back()}
-                        className="rounded-full bg-[#d1e4fa] text-blue-700 flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden h-10 px-4 text-sm font-bold leading-normal tracking-[0.015em] w-full max-w-[480px] lg:w-auto"
-                    >
-                        <ArrowBackIcon className="mr-2" /> {/* Add some margin to the right */}
-                        Go Back
-                    </button>
-                </div>
-                {candidateData ? (
-                    <>
-                        <div className="px-40 flex flex-1 justify-center py-5">
-                            <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
-                                <div className="flex p-4">
-                                    <div className="flex w-full flex-col gap-4 lg:flex-row lg:justify-between lg:items-center">
-                                        <div className="flex gap-4">
-                                            <div className="bg-center bg-no-repeat bg-cover rounded-full h-32 w-32" style={{ backgroundImage: 'url("Strapi.Image - PUT HERE")' }}></div> 
-                                            <div className="flex flex-col justify-center">
-                                                <h1 className="text-6xl pb-2 font-bold mb-2 justify-center text-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">{candidateData?.Name}</h1>
-                                                <p className="text-blue-700 text-base text-4xl font-6xl leading-normal">{candidateData?.Role}</p>
-                                                <p className="text-blue-700 text-base text-4xl font-6xl leading-normal">{candidateData?.Party}</p>
-                                            </div>
-                                        </div>
+            <header className="flex border-b border-solid border-b-white px-10 py-3"></header>
+
+            {/* Go Back button */}
+            <div className="flex mt-6">
+                <button
+                    type="button"
+                    onClick={() => router.back()}
+                    className="rounded-full bg-[#d1e4fa] text-blue-700 flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden h-10 px-4 text-sm font-bold leading-normal tracking-[0.015em] w-full max-w-[480px] lg:w-auto"
+                >
+                    <ArrowBackIcon className="mr-2" /> {/* Add some margin to the right */}
+                    Go Back
+                </button>
+            </div>
+
+            {/* Actual candidate data */}
+            {candidateData ? (
+                <div className="lg:px-40 md:px-20 px-10 flex flex-1 justify-center py-5">
+                    <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
+                        <div className="flex p-4">
+                            <div className="flex w-full flex-col gap-4 lg:flex-row lg:justify-between lg:items-center">
+                                <div className="flex gap-4">
+
+                                    {/* Candidate image */}
+                                    <div className="bg-center bg-no-repeat bg-cover rounded-full h-32 w-32" style={{ backgroundImage: 'url("Strapi.Image - PUT HERE")' }}></div> 
+
+                                    {/* Name, role, party */}
+                                    <div className="flex flex-col justify-center">
+                                        <h1 className="text-6xl pb-2 font-bold mb-2 justify-center text-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">{candidateData?.Name}</h1>
+                                        <p className="text-blue-700 text-2xl font-6xl leading-normal">{candidateData?.Role}</p>
+                                        <p className="text-blue-700 text-2xl font-6xl leading-normal">{candidateData?.Party}</p>
                                     </div>
                                 </div>
-                                <div className='flex flex-col justify-center items-center p-4 text-center my-6'>
-                                    {candidateData.CampaignSiteLink && (
-                                        <ButtonFillEx name='Campaign Site' link={candidateData.CampaignSiteLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800 hover:bg-gray-200' />
-                                    )}
-                                    {candidateData.LinkedinLink && (
-                                        <ButtonFillEx name='Linkedin Link' link={candidateData.LinkedinLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800 hover:bg-gray-200' />
-                                    )}
-                                </div>
-                                
-                                
-                                    {Object.entries(questionsAndAnswers).length > 0 ? (
-                                        <div className="flex flex-col justify-center items-center py-8 px-6 my-6">
-                                        <p className="text-4xl font-semibold mb-8 text-center">Questions curated by the founder, journalist Yawu Miller</p>
-                                        {Object.entries(questionsAndAnswers).map(([index, qa]) => (
-                                            qa.question && qa.answer ? (
-                                                <Accordion key={index} className='bg-white w-full lg:w-3/4 md:w-3/4 mb-3'>
-                                                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`panel${index}-content`} id={`panel${index}-header`}>
-                                                        <Typography className='text-blue-700 text-xl'>{qa.question}</Typography>
-                                                    </AccordionSummary>
-                                                    <AccordionDetails>
-                                                        <Typography className='mb-8 text-xl'>{qa.answer}</Typography>
-                                                    </AccordionDetails>
-                                                </Accordion>
-                                            ) : null
-                                        ))}
-                                        </div>
-                                    ) : null }
-                                </div>
                             </div>
-                    </>
-                ) : (null)}
-            </div>
-      
+                        </div>
+
+                        {/* Links */}
+                        <div className='flex flex-col justify-center items-center p-4 text-center my-6'>
+                            {candidateData.CampaignSiteLink && (
+                                <ButtonFillEx name='Campaign Site' link={candidateData.CampaignSiteLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800 hover:bg-gray-200' />
+                            )}
+                            {candidateData.LinkedinLink && (
+                                <ButtonFillEx name='Linkedin' link={candidateData.LinkedinLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800 hover:bg-gray-200' />
+                            )}
+                        </div>
+
+                        {/* Questions and Answers */}
+                        <div className="flex flex-col justify-center items-center py-8 my-6">
+                        <p className="text-4xl font-semibold mb-8 text-center">Questions curated by the founder, journalist Yawu Miller</p>
+                        {Object.entries(questionsAndAnswers).map(([index, qa]) => (
+                            qa.question && qa.answer ? (
+                                <Accordion key={index} className='bg-white w-full lg:w-3/4 md:w-3/4 mb-3'>
+
+                                    {/* Question */}
+                                    <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`panel${index}-content`} id={`panel${index}-header`}>
+                                        <Typography className='text-blue-700 text-xl'>{qa.question}</Typography>
+                                    </AccordionSummary>
+
+                                    {/* Answer */}
+                                    <AccordionDetails>
+                                        <Typography className='mb-8 text-xl'>{qa.answer}</Typography>
+                                    </AccordionDetails>
+                                </Accordion>
+                            ) : null
+                        ))}
+                        </div>
+                    </div>
+                </div>
+            ) : (null)}
+        </div>
     );
 }
