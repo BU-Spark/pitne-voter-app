a12a21407ee95f0d0e851f7bcc785de876237ff5
jasonjiang9142@gmail.com <jasonjiang9142@gmail.com>
2024-06-26 10:08:29 -0400

    finish adding picture

--- client/src/pages/ballotInfo/[candidate].tsx
+++ client/src/pages/ballotInfo/[candidate].tsx
@@ -32,6 +32,14 @@ interface CandidateAttributes {
     Answer4: string | null;
     Question5: string | null;
     Answer5: string | null;
+    Headshot: {
+        data: {
+            attributes: {
+                url: string
+            }
+        }
+
+    }
 }
 
 interface CandidateDataObject {
@@ -66,7 +74,7 @@ export default function Candidate() {
     useEffect(() => {
         const getData = async () => {
             try {
-                const response = await fetch(localCandidateAPI, {
+                const response = await fetch(localCandidateAPI + '?populate=*', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
@@ -128,54 +136,54 @@ export default function Candidate() {
         <div className="p-4 #d1e4fa ">
             {candidateData ? (
                 <>
-                <h1 className="text-6xl pb-2 font-bold mb-8 justify-center text-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">{candidateData?.Name}</h1>
-                <div className="border-t border-white pt-4">
-                <div className="flex flex-col ">
-                {/* <div className="bg-white rounded-lg shadow-xl p-18 flex flex-col"> */}
-                    
-                    <p className="text-3xl mb-2 font">{candidateData.Party} </p>
-                    <p className="text-xl mb-2">{candidateData.Role}</p>
-                    <p className="text-3xl mb-2">{candidateData.District} </p> {/*if all districts then do not display it*/}
-                    <p className="text-3xl mb-2">{candidateData.ElectionName}</p>
+                    <h1 className="text-6xl pb-2 font-bold mb-8 justify-center text-center bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">{candidateData?.Name}</h1>
+                    <div className="border-t border-white pt-4">
+                        <div className="flex flex-col ">
+                            {/* <div className="bg-white rounded-lg shadow-xl p-18 flex flex-col"> */}
 
-                    <div className='flex flex-col justify-center items-center p-4 text-center my-6'>
-                    {candidateData.CampaignSiteLink && (
-                        <ButtonFillEx name='Campaign Site' link={candidateData.CampaignSiteLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800  hover:bg-gray-200' />
+                            <p className="text-3xl mb-2 font">{candidateData.Party} </p>
+                            <p className="text-xl mb-2">{candidateData.Role}</p>
+                            <p className="text-3xl mb-2">{candidateData.District} </p> {/*if all districts then do not display it*/}
+                            <p className="text-3xl mb-2">{candidateData.ElectionName}</p>
 
-                    )}
-                    {candidateData.LinkedinLink && (
-                        <ButtonFillEx name='Linkedin Link' link={candidateData.LinkedinLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800  hover:bg-gray-200' />                    )}
+                            <div className='flex flex-col justify-center items-center p-4 text-center my-6'>
+                                {candidateData.CampaignSiteLink && (
+                                    <ButtonFillEx name='Campaign Site' link={candidateData.CampaignSiteLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800  hover:bg-gray-200' />
+
+                                )}
+                                {candidateData.LinkedinLink && (
+                                    <ButtonFillEx name='Linkedin Link' link={candidateData.LinkedinLink} className='p-4 m-4 rounded-full bg-white text-blue-700 border-blue-800  hover:bg-gray-200' />)}
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
 
 
-                {/* Questions and answers */}
-                {Object.entries(questionsAndAnswers).length > 0 ? (
-                    <div className="flex flex-col justify-center items-center py-8 px-6 my-6">
-                    <p className="text-2xl font-semibold mb-8 text-center">Questions curated by the founder, journalist Yawu Miller</p>
-                    {Object.entries(questionsAndAnswers).map(([index, qa]) => (
-                        qa.question && qa.answer ? (
-                            <Accordion key={index} className='bg-white w-full lg:w-3/4 md:w-3/4 mb-3'>
+                    {/* Questions and answers */}
+                    {Object.entries(questionsAndAnswers).length > 0 ? (
+                        <div className="flex flex-col justify-center items-center py-8 px-6 my-6">
+                            <p className="text-2xl font-semibold mb-8 text-center">Questions curated by the founder, journalist Yawu Miller</p>
+                            {Object.entries(questionsAndAnswers).map(([index, qa]) => (
+                                qa.question && qa.answer ? (
+                                    <Accordion key={index} className='bg-white w-full lg:w-3/4 md:w-3/4 mb-3'>
 
-                                {/* Question */}
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls={`panel${index}-content`}
-                                    id={`panel${index}-header`}
-                                >
-                                    <Typography className='text-blue-700 text-xl'>{qa.question}</Typography>
-                                </AccordionSummary>
+                                        {/* Question */}
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls={`panel${index}-content`}
+                                            id={`panel${index}-header`}
+                                        >
+                                            <Typography className='text-blue-700 text-xl'>{qa.question}</Typography>
+                                        </AccordionSummary>
 
-                                {/* Answer */}
-                                <AccordionDetails>
-                                    <Typography className='mb-8 text-xl'>{qa.answer}</Typography>
-                                </AccordionDetails>
-                            </Accordion>
-                        ) : null
-                    ))}
-                    </div>
-                ) : null}
+                                        {/* Answer */}
+                                        <AccordionDetails>
+                                            <Typography className='mb-8 text-xl'>{qa.answer}</Typography>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                ) : null
+                            ))}
+                        </div>
+                    ) : null}
                 </>
             ) : null}
         </div>
