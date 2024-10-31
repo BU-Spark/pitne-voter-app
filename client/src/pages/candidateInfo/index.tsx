4b5ecd5f00bf913c257db6bd96cccf65f71b5f4a
Grace Murphy <gamurphy@bu.edu>
2024-10-31 15:08:42 -0400

    Update index.tsx

--- client/src/pages/candidateInfo/index.tsx
+++ client/src/pages/candidateInfo/index.tsx
@@ -8,7 +8,6 @@ interface Candidate {
         District: string;
         Party: string;
         ElectionName: string;
-        Bio?: string;
         Bio: string;
         CampaignSiteLink?: string;
         LinkedInLink?: string;
@@ -19,7 +18,6 @@ interface Candidate {
 
 // Component for Candidate Information Page
 export default function CandidateInfo() {
-    const [candidates, setCandidates] = useState<Candidate[]>([]);
     const [candidates, setCandidates] = useState<Candidate[]>([]); //holds array of candiate objects
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
@@ -28,7 +26,6 @@ export default function CandidateInfo() {
         const fetchCandidateData = async () => {
             console.log("Fetching candidate data...");
             try {
-                const response = await fetch(`https://pitne-voter-app-production.up.railway.app/api/candidates?populate=Headshot`);
                 const response = await fetch(`https://pitne-voter-app-production.up.railway.app/api/candidates`);
                 console.log("API Response Status:", response.status);
 
