b8f296a9e9021b25c2ff789883f9bb8182297eef
freyaazhu <freyaz1119@gmail.com>
2025-04-15 17:20:19 -0400

    small padding change to navbar

--- client/src/components/nav/NavBar.tsx
+++ client/src/components/nav/NavBar.tsx
@@ -99,7 +99,8 @@ const NavBar = () => {
           <Box sx={{ 
             display: 'flex', 
             mr: 1,
-            paddingTop: '12px' // Added padding here
+            paddingTop: '12px', // Added padding here
+            paddingBottom: '10px'
           }}>
             <img 
               src="/BVLogo.svg" 
